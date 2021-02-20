82817433ca3da38085c3338ec763858b549fb5d4
Douglas Lopes <douglasdmlopes@gmail.com>
2021-02-19 23:05:29 -0300

    Implementação das opções de atualizar e deletar tarefas

--- web/src/main.js
+++ web/src/main.js
@@ -7,7 +7,11 @@ const apiTodos = new Todos()
 const app = createApp({
 	data() {
 		return {
-			todos: []
+			todos: [],
+			form: {
+				text: '',
+        done: false
+			}
 		}
 	},
 
@@ -19,6 +23,31 @@ const app = createApp({
     async fetchTodos() {
       this.todos = await apiTodos.index()
       console.log(this.todos)
+    },
+    async createTodo() {
+      const data = await apiTodos.store(this.form)
+      this.todos.push(data)
+
+      this.form.text = ''
+      this.form.done = false
+    },
+    async toggleTodoStatus(todo) {
+      const data = await apiTodos.update({
+        ... todo,
+        done: !todo.done
+      })
+
+      const index = this.todos.findIndex(({id}) => id === data.id)
+
+      this.todos[index] = data
+    },
+
+    async destroyTodo(id) {
+      await apiTodos.destroy({ id })
+
+      const index = this.todos.findIndex((todo) => todo.id === id)
+
+      this.todos.splice(index, 1 )
     }
   }
 })
